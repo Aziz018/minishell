bb898bb3a8a513ee43d94f72eed5c2ff6246eb82
Aziz018 <khatabiaziz707@gmail.com>
2024-04-22 09:32:25 -0400

    minishell

--- minishell.c
+++ minishell.c
@@ -1,23 +1,24 @@
 #include "minishell.h"
 
-int main()
+void print_prompt()
 {
     const char *prompt = "aziz@kali ~/desktop/minishell> ";
     const char *output_color = GREEN_COLOR;
     const char *reset_color = RESET_COLOR;
-
     write(1, output_color, ft_strlen(output_color));
     write(1, prompt, ft_strlen(prompt));
     write(1, reset_color, ft_strlen(reset_color));
+}
 
+int main()
+{
+    print_prompt();
     char *line = get_next_line(0);
     if (line == NULL || line[0] == '\0')
         return 0;
     while(line != NULL)
     {
-        write(1, output_color, ft_strlen(output_color));
-        write(1, prompt, ft_strlen(prompt));
-        write(1, reset_color, ft_strlen(reset_color));
+        print_prompt();
         line = get_next_line(0);
         if (line == NULL || line[0] == '\0')
             return 0;
