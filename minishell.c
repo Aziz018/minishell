c227c89e92017a1edb39cdb2ab1031f1034e743f
Aziz018 <khatabiaziz707@gmail.com>
2024-04-22 09:30:31 -0400

    minishell

--- minishell.c
+++ minishell.c
@@ -2,15 +2,25 @@
 
 int main()
 {
-    char *line = get_next_line(0);
-    // if (line == NULL || line[0] == '\0')
-    //     return 0;
-    printf("%s %d\n", line, line[0]);
-    while(line != NULL || line[0] == '\0')
-    {
-        line = get_next_line(0);
-        printf("%s %d\n", line, line[0]);
+    const char *prompt = "aziz@kali ~/desktop/minishell> ";
+    const char *output_color = GREEN_COLOR;
+    const char *reset_color = RESET_COLOR;
 
+    write(1, output_color, ft_strlen(output_color));
+    write(1, prompt, ft_strlen(prompt));
+    write(1, reset_color, ft_strlen(reset_color));
+
+    char *line = get_next_line(0);
+    if (line == NULL || line[0] == '\0')
+        return 0;
+    while(line != NULL)
+    {
+        write(1, output_color, ft_strlen(output_color));
+        write(1, prompt, ft_strlen(prompt));
+        write(1, reset_color, ft_strlen(reset_color));
+        line = get_next_line(0);
+        if (line == NULL || line[0] == '\0')
+            return 0;
     }
     return 0;
 }
--- minishell.h
+++ minishell.h
@@ -5,5 +5,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define GREEN_COLOR "\033[1;32m"
+#define RESET_COLOR "\033[0m"
 
 #endif
