5f48974671e66919d0793758bd8631ad43b11c1e
Aziz018 <khatabiaziz707@gmail.com>
2024-05-13 14:44:53 +0100

    norm

--- minishell.c
+++ minishell.c
@@ -6,50 +6,50 @@
 /*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:42:13 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/04/23 13:43:04 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/05/13 14:44:33 by aelkheta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void print_prompt()
+void	print_prompt(void)
 {
-    char *prompt = "┌──(aziz㉿hostname)-[~/Desktop/minishell]\n└─$ ";
+	char	*prompt;
+	char	*output_color;
+	char	*reset_color;
 
-    // char *prompt = "aziz@kali ~/desktop/minishell> ";
-    // printf("┌──(aziz㉿hostname)-[~/Desktop/minishell]\n");
-    // printf("└─$ ");
-    char *output_color = GREEN_COLOR;
-    char *reset_color = RESET_COLOR;
-    // printf("%s\n", prompt);
-    write(1, output_color, ft_strlen(output_color));
-    write(1, prompt, ft_strlen(prompt));
-    write(1, reset_color, ft_strlen(reset_color));
+	prompt = "┌──(aziz㉿hostname)-[~/Desktop/minishell]\n└─$ ";
+	output_color = N_GREEN_COLOR;
+	reset_color = RESET_COLOR;
+	write(1, output_color, ft_strlen(output_color));
+	write(1, prompt, ft_strlen(prompt));
+	write(1, reset_color, ft_strlen(reset_color));
 }
 
-int check_command(char *command)
+int	check_command(char *command)
 {
-    if (ft_strnstr(command, "ls", ft_strlen(command)))
-    {
-        system("ls");
-    }
-    return 0;
+	if (ft_strnstr(command, "ls", ft_strlen(command)))
+	{
+		system("ls");
+	}
+	return (0);
 }
 
-int main()
+int	main(void)
 {
-    print_prompt();
-    char *command;
-    command = get_next_line(0);
-    if (command == NULL || command[0] == '\0')
-        return 0;
-    while(1)
-    {
-        if (command == NULL || command[0] == '\0')
-            return 0;
-        check_command(command);
-        print_prompt();
-        command = get_next_line(0);
-    }
-    return 0;
+	char	*command;
+
+	print_prompt();
+	command = get_next_line(0);
+	if (command == NULL || command[0] == '\0')
+		return (0);
+	while (1)
+	{
+		if (command == NULL || command[0] == '\0')
+			return (0);
+		check_command(command);
+		print_prompt();
+		command = get_next_line(0);
+	}
+	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -1,18 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 14:43:39 by aelkheta          #+#    #+#             */
+/*   Updated: 2024/05/13 14:43:40 by aelkheta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#include "libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include "libft/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-#define GREEN_COLOR "\033[1;32m"
-#define RESET_COLOR "\033[0m"
+# define B_RED_COLOR "\033[1;31m"
+# define N_RED_COLOR "\033[0;31m"
+# define B_GREEN_COLOR "\033[1;32m"
+# define N_GREEN_COLOR "\033[0;32m"
+# define RESET_COLOR "\033[0m"
 
-void print_prompt();
-
+void	print_prompt(void);
 
 #endif
