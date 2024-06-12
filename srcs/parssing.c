8402543cd3074538238099fc6cb1dac3d86053e5
Aziz018 <khatabiaziz707@gmail.com>
2024-06-12 10:40:32 -0400

    orgnize functions and fix memory leaks

--- srcs/parssing.c
+++ srcs/parssing.c
@@ -3,46 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   parssing.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 14:40:09 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/06/11 18:44:48 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/06/12 10:32:32 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libraries/minishell.h"
 
-void parentheses(char *command)
+char *skip_white_spaces(char *command)
 {
-	data->i = -1;
-	data->j = -1;
-	while(command[++data->i])
-	{
-		if (command[data->i] == '(')
-			++data->j;
-		else
-		{
-			if (data->j == -1)
-			{
-				printf("minishell: syntax error near unexpected token `)'\n");
-				return ;
-			}
-			if (command[data->i] == ')')
-				--data->j;
-		}
-		if (data->j < -1)
-		{
-			printf("minishell: syntax error near unexpected token `)'\n");
-			return ;
-		}
-	}
-	if (data->j == -1)
-		return ;
-	char *line = readline("> ");
-	data->new_command = ft_strjoin(command, line);
-	free(line);
-	free(command);
-	parentheses(data->new_command);
+	while(*command && (*command == ' ' || *command == '\t' || *command == '\r' || *command == '\v'))
+		command++;
+	return (command);
 }
 
 t_command *tokenize_command(char *commads)
@@ -56,77 +30,13 @@ t_command *tokenize_command(char *commads)
 		return NULL;
 	while(commads[token.i])
 	{
-		{
-			// printf("\ncommads[%d]: %d\n", token.i, commads[token.i]);
-			
-			token.value = get_token_value(&token, commads);
-			if (!token.value)
-				break;
-			token.type = get_token_type(&token);		
-			add_back_list(&head, new_node(token.type, token.value));
-			
-			// free(token.value);
-			
-			// token.type = get_token_type(&token);
-			// new_node(token->type, token->value);
-			// printf("token: --------- %s\n", token.value);
-			// printf("type: ---------- %d\n", token.type);
-			// token->type = get_token_type(token);
-			// printf("\n");
-		}
-		// if (token != NULL)
-		// {
-			// if (token.value != NULL)
-			// 	free(token.value);
-			// free(token);
-		// }
+		token.value = get_token_value(&token, commads);
+		if (!token.value)
+			break;
+		token.type = get_token_type(&token);		
+		add_back_list(&head, new_node(token.type, token.value));
 	}
 	return (head);
-}
-
-char *skip_white_spaces(char *command)
-{
-	while(*command && (*command == ' ' || *command == '\t' || *command == '\r' || *command == '\v'))
-		command++;
-	return (command);
-}
-
-char *skip_command(char *command)
-{
-	while(*command && ft_isalnum(*command))
-		command++;
-	command = skip_white_spaces(command);
-	if (*command == ';')
-		command++;
-	return (command);
-}
-
-int check_syntax(char *command)
-{
-	while(*command)
-	{
-		command = skip_white_spaces(command);
-		if (!*command)
-			break;
-		if (*command == ';')
-			printf("minishell: syntax error near unexpected token `%c'\n", *command);
-		command = skip_command(command);
-		if (!*command)
-			break;
-		
-		printf("%s\n", command);
-		
-		if (*command == ';')
-		{
-			command = skip_white_spaces(command);
-			if (!*command)
-				break;
-			else if (*command == ';')
-				printf("minishell: syntax error near unexpected token `%c'\n", *command);
-		}
-		command++;
-	}
-	return (0);
 }
 
 int get_args_size(t_command *token)
@@ -161,13 +71,6 @@ t_command *parser_command(t_command *tokens)
 	t_command *ptr = tokens;
 	t_command *temp = tokens;
 	temp->args = NULL;
-	
-	// if (data->syntax_error)
-	// {
-	// 	clear_list(&tokens);
-	// 	return (0);
-	// }
-	
 	while(ptr != NULL)
 	{
 		temp = ptr;
@@ -175,22 +78,19 @@ t_command *parser_command(t_command *tokens)
 		temp->args = NULL;
 		if (!ptr)
 			break;
-		if (temp->type == CMD)
+		if (temp->type == CMD && ptr->type == ARG)
 		{
-			if (ptr->type == ARG)
+			int i = 0;
+			temp->args = malloc((get_args_size(ptr) + 2) * sizeof(char *));
+			temp->args[i++] = ft_strdup(temp->value);
+			while(ptr != NULL && ptr->type == ARG)
 			{
-				int i = 0;
-				temp->args = malloc((get_args_size(ptr) + 2) * sizeof(char *));
-				temp->args[i++] = ft_strdup(temp->value);
-				while(ptr != NULL && ptr->type == ARG)
-				{
-					temp->args[i++] = ptr->value;
-					t_command *aziz = ptr->next;
-					free(ptr);
-					ptr = aziz;
-				}
-				temp->args[i] = NULL;
+				temp->args[i++] = ptr->value;
+				t_command *aziz = ptr->next;
+				free(ptr);
+				ptr = aziz;
 			}
+			temp->args[i] = NULL;
 			temp->next = ptr;
 		}
 		else if (temp->type == RED_OUT || temp->type == RED_IN || temp->type == APP || temp->type == HER_DOC)
@@ -201,7 +101,6 @@ t_command *parser_command(t_command *tokens)
 			t_command *aziz = ptr->next;
 			free(ptr);
 			ptr = aziz;
-			// ptr = ptr->next;
 			temp->next = ptr;
 		}
 	}
@@ -216,87 +115,36 @@ void print_list(t_command *tokens)
 	{
 		printf("=> node: %d\n+---------------------------+\n| token: ---------- %s\n", ++i, tokens->value);
 		if (tokens->type == CMD)
-		{
-			// int i = 0;
 			printf("| type: ----------- CMD     |\n+---------------------------+\n");
-			print_args(tokens);
-			printf("\n");
-		}
-			
-		if (tokens->type == RED_OUT)
+		else if (tokens->type == RED_OUT)
 			printf("| type: ----------- RED_OUT |\n+---------------------------+\n\n");
-		if (tokens->type == RED_IN)
+		else if (tokens->type == RED_IN)
 			printf("| type: ----------- RED_IN  |\n+---------------------------+\n\n");
-		if (tokens->type == APP)
+		else if (tokens->type == APP)
 			printf("| type: ----------- APP     |\n+---------------------------+\n\n");
-		if (tokens->type == HER_DOC)
+		else if (tokens->type == HER_DOC)
 			printf("| type: ----------- HER_DOC |\n+---------------------------+\n\n");
-		if (tokens->type == PIPE)
+		else if (tokens->type == PIPE)
 			printf("| type: ----------- PIPE    |\n+---------------------------+\n\n");
-		if (tokens->type == LIST)
+		else if (tokens->type == LIST)
 			printf("| type: ----------- LIST    |\n+---------------------------+\n\n");
-		if (tokens->type == BACK)
+		else if (tokens->type == BACK)
 			printf("| type: ----------- BACK    |\n+---------------------------+\n\n");
-		if (tokens->type == ARG)
+		else if (tokens->type == ARG)
 			printf("| type: ----------- ARG     |\n+---------------------------+\n\n");
-		if (tokens->type == OR_OP)
+		else if (tokens->type == OR_OP)
 			printf("| type: ----------- OR_OP   |\n+---------------------------+\n\n");
-		if (tokens->type == AND_OP)
+		else if (tokens->type == AND_OP)
 			printf("| type: ----------- AND_OP  |\n+---------------------------+\n\n");
-		if (tokens->type == FLE)
+		else if (tokens->type == FLE)
 			printf("| type: ----------- FLE     |\n+---------------------------+\n\n");
-		if ((tokens->type == RED_OUT || tokens->type == RED_IN || tokens->type == APP || tokens->type == HER_DOC) && tokens->args != NULL)
-		{
-			printf("	+---------------------------+\n");
-			printf("	| arg[%d]: ----------- [%s]\n", 1, tokens->args[0]);
-			printf("	+---------------------------+\n");
-			printf("\n");
-			free(tokens->args[0]);
-			free(tokens->args);
-		}
-		
+			
+		print_args(tokens);
 		t_command *tmp = tokens;
 		tokens = tokens->next;
 		free(tmp->value);
 		free(tmp);
-		// tokens = tmp;
 	}
-
-
-		// for debuging:
-	
-	// while(tokens != NULL)
-	// {
-	// 	printf("=> node: %d\n+---------------------------+\n| token: ---------- %s\n", ++i, tokens->value);
-	// 	if (tokens->type == CMD)
-	// 		printf("| type: ----------- CMD     |\n+---------------------------+\n\n");
-	// 	if (tokens->type == RED_OUT)
-	// 		printf("| type: ----------- RED_OUT |\n+---------------------------+\n\n");
-	// 	if (tokens->type == RED_IN)
-	// 		printf("| type: ----------- RED_IN  |\n+---------------------------+\n\n");
-	// 	if (tokens->type == PIPE)
-	// 		printf("| type: ----------- PIPE    |\n+---------------------------+\n\n");
-	// 	if (tokens->type == LIST)
-	// 		printf("| type: ----------- LIST    |\n+---------------------------+\n\n");
-	// 	if (tokens->type == BACK)
-	// 		printf("| type: ----------- BACK    |\n+---------------------------+\n\n");
-	// 	if (tokens->type == ARG)
-	// 		printf("| type: ----------- ARG     |\n+---------------------------+\n\n");
-	// 	if (tokens->type == OR_OP)
-	// 		printf("| type: ----------- OR_OP   |\n+---------------------------+\n\n");
-	// 	if (tokens->type == AND_OP)
-	// 		printf("| type: ----------- AND_OP  |\n+---------------------------+\n\n");
-	// 	if (tokens->type == FLE)
-	// 		printf("| type: ----------- FLE     |\n+---------------------------+\n\n");
-	// 	if (tokens->type == APP)
-	// 		printf("| type: ----------- APP     |\n+---------------------------+\n\n");
-	// 	t_command *tmp = tokens->next;
-	// 	free(tokens->value);
-	// 	free(tokens); 
-	// 	tokens = tmp;	
-	// 	// tokens = tokens->next;
-	// }
-
 }
 
 int	parse_command(char *command)
--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -3,19 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelkheta <aelkheta@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:51:08 by aelkheta          #+#    #+#             */
-/*   Updated: 2024/06/10 18:10:15 by aelkheta         ###   ########.fr       */
+/*   Updated: 2024/06/12 10:34:14 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libraries/minishell.h"
 
+int set_token_type(t_token *token, int type)
+{
+    token->prev_type = type;
+	token->type = type;
+	return (type);
+}
+
 int syntax_error(char *command)
 {
 	int i = 0;
-	if (ft_strchr("<|>&;", command[i])) //<|>&;
+	if (ft_strchr("<|>&;", command[i]))
 	{
 		char special = command[i];
 		while(command[++i] && ft_strchr(" \t\v", command[i])); // ++i to skip the char and start from the char after
@@ -39,38 +46,9 @@ char *get_token_value(t_token *token, char *command)
 	if (command[token->i] == '\0')
 		return (NULL);
 	token->index = token->i;
-	// if (ft_strchr("'\"[{(", command[token->i]))
-	// {
-	// 	char special = command[token->i];
-	// 	if (command[token->i] == '[')
-	// 		special = ']';
-	// 	else if (command[token->i] == '{')
-	// 		special = '}';
-	// 	else if (command[token->i] == '(')
-	// 		special = ')';
-		
-	// 	// token->i++;
-	// 	while(command[++token->i] && command[token->i] == special);
-	// 		// token->i++;
-	// 	token->index = token->i;
-	// 	while(command[token->i] && command[token->i] != special)
-	// 		token->i++;
-	// 	if (!command[token->i])
-	// 	{
-	// 		write(2, "minishell: syntax error near unexpected token ';'\n", 51);
-	// 		data->syntax_error = true;
-	// 		return NULL;
-	// 	}
-	// 	while(command[++token->i] && ft_isalnum(command[token->i]));
-	// 	token_val = malloc((token->i - token->index) * sizeof(char) + 1);
-	// 	ft_strlcpy(token_val, &command[token->index], (token->i - token->index + 1));
-	// 	token->index = token->i;
-	// 	return (token_val);
-	// }
+	
 	if (command[token->i] && ft_strchr("<|>&;", command[token->i]))
 	{
-		// if (syntax_error(&command[token->i]))
-		// 	return (NULL);
 		if ((command[token->i] == '<' && command[token->i + 1] == '<') || (command[token->i] == '>' && command[token->i + 1] == '>') || (command[token->i] == '|' && command[token->i + 1] == '|') || (command[token->i] == '&' && command[token->i + 1] == '&'))
 		{
 			token_val = malloc(3 * sizeof(char));
@@ -99,14 +77,6 @@ char *get_token_value(t_token *token, char *command)
 	return (token_val);
 }
 
-
-int set_token_type(t_token *token, int type)
-{
-    token->prev_type = type;
-	token->type = type;
-	return (type);
-}
-
 int get_token_type(t_token *token)
 {
 	if (token->value[0] == '|' && token->value[1] == '\0')
@@ -131,91 +101,8 @@ int get_token_type(t_token *token)
 		return (set_token_type(token, ARG));
 	else if (token->prev_type == RED_IN || token->prev_type == RED_OUT || token->prev_type == APP || token->prev_type == HER_DOC)
 		return (set_token_type(token, FLE));
-	else //if (token->prev_type == -1 || token->prev_type == PIPE || token->prev_type == RED_IN || token->prev_type == LIST)
+	else 
 		return (set_token_type(token, CMD));
 	return (0);
 }
 
-
-// int get_token_type(t_token *token)
-// {
-// 	if (token->value[0] == '|' && token->value[1] == '\0')
-// 	{
-// 		// token->type = PIPE;
-// 		// token->prev_type = PIPE;
-// 		// // printf("type: ---------- PIPE\n");
-// 		// return (PIPE);
-// 		return (set_token_type(token, PIPE));
-// 	}
-// 	else if (token->value[0] == '>' && token->value[1] == '\0')
-// 	{
-// 		token->type = RED_OUT;
-// 		token->prev_type = RED_OUT;
-// 		// printf("type: ---------- RED_OUT\n");
-// 		return(RED_OUT);
-// 	}
-// 	else if (token->value[0] == '>' && token->value[1] == '>' && token->value[2] == '\0')
-// 	{
-// 		token->type = APP;
-// 		token->prev_type = APP;
-// 		// printf("type: ---------- APP\n");
-// 		return(APP);
-// 	}
-// 	else if (token->value[0] == '<' && token->value[1] == '\0')
-// 	{
-// 		token->type = RED_IN;
-// 		token->prev_type = RED_IN;
-// 		// printf("type: ---------- RED_IN\n");
-// 		return(RED_IN);
-// 	}
-// 	else if (token->value[0] == '&' && token->value[1] == '\0')
-// 	{
-// 		token->type = BACK;
-// 		token->prev_type = BACK;
-// 		// printf("type: ---------- BACK\n");
-// 		return(BACK);
-// 	}
-// 	else if (token->value[0] == '&' && token->value[1] == '&' && token->value[2] == '\0')
-// 	{
-// 		token->type = AND_OP;
-// 		token->prev_type = AND_OP;
-// 		// printf("type: ---------- AND_OP\n");
-// 		return(AND_OP);
-// 	}
-// 	else if (token->value[0] == '|' && token->value[1] == '|' && token->value[2] == '\0')
-// 	{
-// 		token->type = OR_OP;
-// 		token->prev_type = OR_OP;
-// 		// printf("type: ---------- OR_OP\n");
-// 		return(OR_OP);
-// 	}
-// 	else if (token->value[0] == ';')
-// 	{
-// 		token->type = LIST;
-// 		token->prev_type = LIST;
-// 		// printf("type: ---------- CMD\n");
-// 		return(LIST);
-// 	}
-// 	else if (token->prev_type == CMD || token->prev_type == ARG)
-// 	{
-// 		token->type = ARG;
-// 		token->prev_type = ARG;
-// 		// printf("type: ---------- ARG\n");
-// 		return(ARG);
-// 	}
-// 	else if (token->prev_type == RED_IN || token->prev_type == RED_OUT || token->prev_type == APP)
-// 	{
-// 		token->type = FLE;
-// 		token->prev_type = FLE;
-// 		// printf("type: ---------- FILE\n");
-// 		return(FLE);
-// 	}
-// 	else if (token->prev_type == -1 || token->prev_type == PIPE || token->prev_type == RED_IN || token->prev_type == LIST)
-// 	{
-// 		token->type = CMD;
-// 		token->prev_type = CMD;
-// 		// printf("type: ---------- CMD\n");
-// 		return(CMD);
-// 	}
-// 	return (0);
-// }
